5e7b2067c914aa42aeb6db4ac57d71ea4ee25dec
barbara carvalho de souza <barbara.carvsouza07@gmail.com>
2023-04-20 10:30:40 -0300

    kbyte para megabyte

---
+++ Kbyteparamegabyte.py
@@ -0,0 +1,12 @@
+def kbyteparamegabyte(value):
+    print('valor kbyte para megabyte')
+    return float(value)
+
+def kbyteparamegabyte(valorASerConvertido):
+    print('resultado')
+    kbyteCalculado = valorASerConvertido / 8
+    return kbyteCalculado
+
+print('insira o valor')
+entradaDoTecladoValorASerConvertido  = kbyteparamegabyte(int(input()))
+print(entradaDoTecladoValorASerConvertido)
