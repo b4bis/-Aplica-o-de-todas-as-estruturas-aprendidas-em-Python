a38bc9fb5d54833a7fc57697ce2fe484df4a5919
barbara carvalho de souza <barbara.carvsouza07@gmail.com>
2023-04-20 10:46:05 -0300

    terabyte para petabyte

---
+++ terabyteparapetabyte.py
@@ -0,0 +1,12 @@
+def terabyteparapetabyte(value):
+    print('terabyte para petabyte')
+    return float(value)
+
+def terabyteparapetabyte(valorASerConvertido):
+    print('resultado')
+    kbyteCalculado = valorASerConvertido / 8
+    return kbyteCalculado
+
+print('insira o valor')
+entradaDoTecladoValorASerConvertido  = terabyteparapetabyte(int(input()))
+print(entradaDoTecladoValorASerConvertido)
